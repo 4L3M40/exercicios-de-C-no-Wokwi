33089079081f016d46e846b2e9d2fe69d0b2df45
Evandro Wagencknecht <evandrobch@hotmail.com>
2025-03-19 16:07:59 -0300

    Update sketch.ino

--- 02/sketch.ino
+++ 02/sketch.ino
@@ -1,19 +1,17 @@
+/*02) Faça um LED piscar a cada segundo na ESP32 usando a linguagem C.*/
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  Serial.println("Hello, ESP32!");
+   Serial.begin(115200);
+   Serial.println("Hello, ESP32!");
 
 pinMode(14, OUTPUT);
 
 }
 
 void loop() {
-  
-digitalWrite(14, HIGH);
-delay(500); 
-digitalWrite(14, LOW);
-delay(1000);
-
+  digitalWrite(14, HIGH);
+  delay(500); 
+  digitalWrite(14, LOW);
+  delay(1000);
 }
 
