d7cccb74a3c553b80113481a443bfb942416756b
Evandro Wagencknecht <evandrobch@hotmail.com>
2025-03-19 16:07:12 -0300

    Update sketch.ino

--- 01/sketch.ino
+++ 01/sketch.ino
@@ -1,6 +1,9 @@
+/* 01) Escreva um programa em C que utilize as capacidades da ESP32 para imprimir 
+uma contagem de 1 a 10 no console serial, com um intervalo de 1 segundo entre c
+ada número. */
+
 #include <Arduino.h>
 
-// Exercicio 01: Contagem de 1 a 10
 void setup() {
     Serial.begin(115200);
 }
